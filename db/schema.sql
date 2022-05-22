4ce41581af588558ba36dbdf145c7edbf019cf47
Alknight17 <knightalex17@gmail.com>
2022-05-22 17:24:58 -0400

    build out db and create tables

---
+++ db/schema.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS employeeDb;
+CREATE DATABASE employeeDb;
+USE employeeDb;
+
+
+/* create department table */
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NULL,
+    PRIMARY KEY (id)
+);
+
+/* create role table*/
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NULL,
+    salary DECIMAL(10) NULL,
+    department_id INT NULL,
+    PRIMARY KEY (id)
+);
+
+/* create employee table */
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NULL,
+    last_name VARCHAR(30) NULL,
+    role_id INT NULL,
+    manager_id INT NULL,
+    PRIMARY KEY (id)
+);
